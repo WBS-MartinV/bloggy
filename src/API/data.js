6ff1100a61390b8d9d2bf044898a67744aff131c
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-05-03 17:37:03 +0200

    add components for blog and comment / comment section
    add api to post comments

--- src/API/data.js
+++ src/API/data.js
@@ -1,58 +1,112 @@
 import credentials from "./credentials.json";
 const contentful = require("contentful");
 
+const { space, environment } = credentials;
+
 const client = contentful.createClient(credentials);
-export default client
 
 const getAllData = async () => {
     const data = await client.getEntries({
         // EVERYTHING!
     });
 
-    console.log('All', data);
-    return data.items
+    console.log("All", data);
+    return data.items;
 };
-
 
 const getBlogs = async () => {
     const data = await client.getEntries({
-        content_type: 'blog',     
+        content_type: "blog",
     });
-    console.log('Blogs', data);
-    return data.items
+    console.log("Blogs", data);
+    return data.items;
 };
 
 const getAuthors = async () => {
     const data = await client.getEntries({
-        content_type: 'author',
-        select: 'sys.createdAt,fields.bio,fields.name,fields.description',  
+        content_type: "author",
+        select: "sys.createdAt,fields.bio,fields.name,fields.description",
     });
-    console.log('Authors', data);
-    return data.items
+    console.log("Authors", data);
+    return data.items;
 };
 
 const getComments = async () => {
     const data = await client.getEntries({
-        content_type: 'comment'        
+        content_type: "comment",
     });
-    console.log('Comments', data);
-    return data.items
-
-}
+    console.log("Comments", data);
+    return data.items;
+};
 
 const getCommentsByArticle = async (id) => {
     const data = await client.getEntries({
-        content_type: 'comment',
-        'fields.replyTo.sys.id': id        
+        content_type: "comment",
+        "fields.replyTo.sys.id": id,
     });
-    console.log('Comments by article ' + id, data);
-    return data.items
-}
+    console.log("Comments by article " + id, data);
+    return data.items;
+};
+
+//////
+const contentful_management = require("contentful-management");
+
+const postingClient = contentful_management.createClient({
+    accessToken: credentials.tokenForPosting,
+});
+
+// activate content type
+postingClient
+    .getSpace(space)
+    .then((space) => space.getEnvironment(environment))
+    .then((environment) => environment.getContentType("comment"))
+    .then((contentType) => contentType.publish());
+
+const postComment = (text, replyTo, author) => {
+    postingClient
+        .getSpace(space)
+        .then((space) => space.getEnvironment(environment))
+        .then((environment) => {
+            console.log(environment);
+            environment
+                .createEntry("comment", {
+                    fields: {
+                        text: {
+                            "en-US": text,
+                        },
+                        author: {
+                            "en-US": {
+                                sys: {
+                                    type: "Link",
+                                    linkType: "Entry",
+                                    id: author,
+                                },
+                            },
+                        },
+                        replyTo: {
+                            "en-US": {
+                                sys: {
+                                    type: "Link",
+                                    linkType: "Entry",
+                                    id: replyTo,
+                                },
+                            },
+                        },
+                    },
+                })
+                .then(entry => entry.publish());
+        });
+};
+
+export default client;
 
 export {
+    //  get data
     getAllData,
     getAuthors,
     getComments,
     getCommentsByArticle,
-    getBlogs
-}
+    getBlogs,
+    //  send data
+    postComment,
+};
--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 // import { getAllData, getBlogs, getComments, getAuthors, getCommentsByArticle } from "./API/data";
-import * as API from './API/data';
-import CLIENT from './API/data';
+import * as API from "./API/data";
+import CLIENT from "./API/data";
 
 import "./App.css";
 
-CLIENT.getEntries().then((response) => console.log('Client:', response));
+CLIENT.getEntries().then((response) => console.log("Client:", response));
 
-const { getBlogs, getComments, getAuthors } = API
+const { getBlogs, getComments, getAuthors } = API;
 
 const Json = (data, index) => (
     <li key={index}>
@@ -17,33 +17,112 @@ const Json = (data, index) => (
     </li>
 );
 
-const extractAuthorData = (data) => data 
-    ? ({
-        name: data?.fields?.name,
-        description: data?.fields?.description,
-        bio: documentToReactComponents(data?.fields?.bio),
-        joined: (new Date(data?.sys?.createdAt)).toLocaleDateString(),
-    })
-    : {
-        loading: true
-    }
+const extractAuthorData = (data) =>
+    data
+        ? {
+              name: data?.fields?.name,
+              description: data?.fields?.description,
+              bio: documentToReactComponents(data?.fields?.bio),
+              joined: new Date(data?.sys?.createdAt).toLocaleDateString(),
+          }
+        : {
+              loading: true,
+          };
 
 const Author = (data) => {
     const { name, description, bio, joined } = extractAuthorData(data);
     // console.log(data, name, description, bio, joined)
     return (
-        <div style={{
-            border: '1px solid black',
-            padding: '1rem',
-            margin: '1rem',
-        }}>
+        <div
+            style={{
+                border: "1px solid black",
+                padding: "1rem",
+                margin: "1rem",
+            }}
+        >
             <h1>{name}</h1>
             <h3>{description}</h3>
-            <div style={{border: '1px solid grey'}}>
+            <div style={{ border: "1px solid grey" }}>
                 <h3>Bio</h3>
                 {bio}
             </div>
             <p>Blogging since {joined}</p>
+        </div>
+    );
+};
+
+const extractBlogData = (data) =>
+    data
+        ? {
+              title: data?.fields?.title,
+              article: documentToReactComponents(data?.fields?.article),
+              published: new Date(data?.fields?.published).toLocaleDateString(),
+              articleID: data.sys.id,
+              authorID: "2TGcpW4pMxZXOa3zJVDoWc",
+              comments: data.comments,
+          }
+        : {
+              loading: true,
+          };
+
+const Blog = (data) => {
+    const { title, article, published } = extractBlogData(data);
+
+    return (
+        <div className="blog">
+            <h1>{title}</h1>
+            <div className="article">{article}</div>
+            <div className="footer">Published on {published}</div>
+        </div>
+    );
+};
+
+const extractCommentData = (data) =>
+    data
+        ? {
+              text: data?.fields?.text,
+              author: data?.fields?.author?.fields?.name,
+              posted: new Date(data?.sys?.createdAt).toLocaleDateString(),
+          }
+        : {};
+const Comment = (data) => {
+    const { text, author, posted } = extractCommentData(data);
+
+    return (
+        <div className="comment">
+            <p>{text}</p>
+            <p>
+                posted by {author} on {posted}
+            </p>
+        </div>
+    );
+};
+
+const CommentSection = ({ comments, articleID, authorID }) => {
+    const [comment, setComment] = useState("");
+
+    return (
+        <div className="comments">
+            <h1>Comment Section!</h1>
+            <form>
+                <textarea
+                    value={comment}
+                    placeholder="Anything to add?"
+                    onChange={(e) => setComment(e.target.value)}
+                ></textarea>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        API.postComment(comment, articleID, authorID);
+                    }}
+                >
+                    Comment!
+                </button>
+                <p>
+                    commenting as {authorID} on {articleID}
+                </p>{" "}
+                {comments.map(Comment)}
+            </form>
         </div>
     );
 };
@@ -53,22 +132,37 @@ function App() {
     const [authors, setAuthors] = useState([]);
     const [comments, setComments] = useState([]);
 
+    const [user, setUser] = useState();
+    const [article, setArticle] = useState();
+
+    const fetchData = async () => {
+        const authors = await getAuthors();
+        const blogs = await getBlogs();
+        const comments = await getComments();
+
+        setAuthors(authors);
+        setBlogs(blogs);
+        setComments(comments);
+
+        setUser(authors[0].sys.id); //Fake login
+        setArticle(blogs[0].sys.id); //Fake selected article
+    };
+
     useEffect(() => {
-        (async () => {
-            API.getAllData(); // just to log
-            API.getCommentsByArticle('4Q01PyViSZIYwtTWlearkP');
-            setAuthors(await getAuthors());
-            setBlogs(await getBlogs());
-            setComments(await getComments());
-        })();
+        fetchData();
     }, []);
 
     return (
         <div className="App">
-            <Author {...authors[0]} />
             Authors: <ol>{authors.map(Author)}</ol>
-            Blogs: <ol>{blogs.map(Json)}</ol>
-            Comments: <ol>{comments.map(Json)}</ol>
+            Blogs:<ol>{blogs.map(Blog)}</ol>
+            Comments: <ol>{comments.map(Comment)}</ol>
+            CommentSection:{" "}
+            <CommentSection
+                comments={comments}
+                articleID={article}
+                authorID={user}
+            ></CommentSection>
         </div>
     );
 }
